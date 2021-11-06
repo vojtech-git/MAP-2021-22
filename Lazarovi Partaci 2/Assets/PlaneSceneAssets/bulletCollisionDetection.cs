e8a32d6186dad4b39ed4b02c17522153b9848842
Mikrovlnka <skore473@gmail.com>
2021-11-06 18:05:56 +0100

    6.11.2021 18:05

--- Lazarovi Partaci 2/Assets/PlaneSceneAssets/bulletCollisionDetection.cs
+++ Lazarovi Partaci 2/Assets/PlaneSceneAssets/bulletCollisionDetection.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class bulletCollisionDetection : MonoBehaviour
 {
-    //    private void Start()
-    //    {
-    //        Physics.IgnoreLayerCollision(8, 9);
-    //    }
+        private void Start()
+        {
+            Physics.IgnoreLayerCollision(0, 9);
+             
+        }
 
     // Start is called before the first frame update
     void OnTriggerEnter(Collider col) {
         //Physics.IgnoreLayerCollision() PRIDAT PRO IGNOROVANI PLAYERA A NABOJE
-        Physics.IgnoreLayerCollision(8, 8, true);
+        Physics.IgnoreLayerCollision(9, 9, true);
+        Physics.IgnoreLayerCollision(0, 9, true);
 
         if (col.gameObject != null) {
             if (col.gameObject.tag == "Enemy") {
@@ -24,7 +26,7 @@ public class bulletCollisionDetection : MonoBehaviour
                 //enemyHealth.enemyHP-=40;
                 Destroy(this.gameObject);  //FUNGUJE ODEBIRANI HP NEPRATELUM
             }
-            if (col.gameObject.layer == 8)
+            if (col.gameObject.layer == 9)
             {
                 Physics.IgnoreCollision(this.gameObject.GetComponent<Collider>(), this.gameObject.GetComponent<Collider>());
                 Debug.Log("NABOJ NABOJ MYJE");
@@ -33,6 +35,7 @@ public class bulletCollisionDetection : MonoBehaviour
             else {
                 Destroy(this.gameObject);
             }
+        
 
         }
 
