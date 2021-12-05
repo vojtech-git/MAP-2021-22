13fdb1dd4fd5be28bfd4cbbb985c3325b71586e4
JanProchazka123 <prochaajan@gmail.com>
2021-12-05 19:02:28 +0100

    Main Corridor, Shooting, Darkness
    no hangar sry :(

--- Lazarovi Partaci 2/Assets/Honza_Scripts/GunScript.cs
+++ Lazarovi Partaci 2/Assets/Honza_Scripts/GunScript.cs
@@ -1,21 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GunScript : MonoBehaviour
 {
+    [Header ("Weapon Stats")]
     public int damage;
     public float timeBetweenShooting, spread, range, reloadTime, timeBetweenShots;
     public int magazineSize, bulletsPerTap;
     public bool allowButtonHold;
     public int bulletsLeft, bulletsShot;
+    public int bulletsMags;
 
     bool shooting, readyToShoot, reloading;
 
+    [Header("Assignebles")]
     public Camera PlayerCam;
     public Transform attackPoint;
     public RaycastHit rayHit;
     public LayerMask whatIsEnemy;
+    public Text AmmoCount;
 
     private void Awake()
     {
@@ -25,13 +30,14 @@ public class GunScript : MonoBehaviour
     private void Update()
     {
         MyInput();
+        AmmoCount.text = ""+ bulletsLeft;
     }
     private void MyInput()
     {
         if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
         else shooting = Input.GetKeyDown(KeyCode.Mouse0);
 
-        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading) Reload();
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading && bulletsMags - magazineSize >= 0) Reload();
 
         //Shoot
         if(readyToShoot && shooting && !reloading && bulletsLeft > 0)
@@ -82,6 +88,7 @@ public class GunScript : MonoBehaviour
     }
     private void ReloadFinished()
     {
+        bulletsMags = bulletsMags - magazineSize;
         bulletsLeft = magazineSize;
         reloading = false;
     }
