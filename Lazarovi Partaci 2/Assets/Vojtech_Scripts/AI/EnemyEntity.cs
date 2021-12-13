60e62c70ea666cde26bcc21e79740b4e75ba38b8
Vojtech Pejsar <pejsarvojtech@gmail.com>
2021-12-13 18:13:54 +0100

    Přesunutí složky / EnemyEntity
    Do enemy entity jsem udelal drop logiku.

--- Lazarovi Partaci 2/Assets/Vojtech_Scripts/AI/EnemyEntity.cs
+++ Lazarovi Partaci 2/Assets/Vojtech_Scripts/AI/EnemyEntity.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 public class EnemyEntity : Entity
 {
     public int enemyEntityId;
-
+    public GameObject HealthKitPrefab;
+    public GameObject[] MoneyPrefabs;
+ 
     public override void Die()
     {
-        base.Die();
         QuestingSystem.Instance.ProgressQuests(GoalType.Kill, enemyEntityId);
+        DropLoot();
+        base.Die();
+    }
+
+    public void DropLoot()
+    {
+        int random = Random.Range(0, 2);
+
+        Debug.Log(random);
+
+        if (random == 0)
+        {
+            Instantiate(HealthKitPrefab, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(MoneyPrefabs[Random.Range(0, MoneyPrefabs.Length)], transform.position, Quaternion.identity);
+        }
     }
 }
