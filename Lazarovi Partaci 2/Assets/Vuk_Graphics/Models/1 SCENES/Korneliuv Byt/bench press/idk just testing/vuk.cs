dbebc319f02bf1a2f07ff005e23488f751703b9c
vuccix <djukanovic.vuk@gmail.com>
2022-01-23 18:35:49 +0100

    23.1.2022 update scén + odstranění toho co mi pushnul vojta
    prejmenoval jsem ty slozky kam se to generovalo vzdycky tak snad uz se to dit nebude, kdyby jo tak uz to nepushuje xd a proste to discardnete (nevim jestli tohle vubec nekdo cte ale kdyby jo tak to tady mate lmao)

---
+++ Lazarovi Partaci 2/Assets/Vuk_Graphics/Models/1 SCENES/Korneliuv Byt/bench press/idk just testing/vuk.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class vuk : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject anim;
+    public GameObject cam2;
+    public GameObject controls;
+
+    public Text repsText;
+    public Text cashText;
+    public int numOfReps;
+    public int cashEarned;
+
+    private void Start()
+    {
+        numOfReps = 0;
+        cashEarned = 0;
+
+        controls.SetActive(true);
+        StartCoroutine(africanAmericansInParis());
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Start();
+
+            player.SetActive(false);
+            cam2.SetActive(true);
+            
+            repsText.text = "Number of reps: 0";
+            cashText.text = "Cash earned: +0";
+        }
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && cam2.activeSelf)
+        {
+            cam2.SetActive(false);
+            player.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && cam2.activeSelf && numOfReps < 100)
+        {
+            anim.GetComponent<Animator>().SetTrigger("Bench");
+
+            numOfReps++;
+            repsText.text = "Number of reps: " + numOfReps;
+
+            switch (numOfReps)
+            {
+                case 25:
+                    cashEarned = 75;
+                    cashText.text = "Cash earned: +" + cashEarned;
+                    break;
+                case 50:
+                    cashEarned = 150;
+                    cashText.text = "Cash earned: +" + cashEarned;
+                    break;
+                case 69:
+                    cashEarned = 300;
+                    cashText.text = "Cash earned: +" + cashEarned;
+                    break;
+                case 100:
+                    cashEarned = 500;
+                    cashText.text = "Cash earned: +" + cashEarned;
+                    repsText.text = "Number of reps: 100";
+
+                    StartCoroutine(prosimNemlatMeHonzo());
+                    break;
+            }
+        }
+    }
+
+    IEnumerator prosimNemlatMeHonzo()
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        anim.GetComponent<AudioSource>().Play();
+        yield return new WaitForSecondsRealtime(5f);
+        cam2.SetActive(false);
+        player.SetActive(true);
+    }
+
+    IEnumerator africanAmericansInParis()
+    {
+        yield return new WaitForSecondsRealtime(5f);
+        controls.SetActive(false);
+    }
+}
