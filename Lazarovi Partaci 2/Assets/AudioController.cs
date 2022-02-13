5bdd957bb4b79595fc113a8f6d4188275718f023
Mikrovlnka <skore473@gmail.com>
2022-02-13 16:59:38 +0100

    click

--- Lazarovi Partaci 2/Assets/AudioController.cs
+++ Lazarovi Partaci 2/Assets/AudioController.cs
@@ -2,13 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickHoverEffect : MonoBehaviour
+public class AudioController : MonoBehaviour
 {
-    
     public AudioSource hoverSound;
     public AudioSource clickSound;
-
-    public void HoverSound(){
+  public void HoverSound(){
         hoverSound.Play();
     }
      public void ClickSound(){
